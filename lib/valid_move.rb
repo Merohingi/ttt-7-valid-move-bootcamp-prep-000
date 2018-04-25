9ebc1bef134629d2639db0d94cdebf3ac63e282d
Marvin Bonds <mbonds1@mail.depaul.edu>
2018-04-25 06:32:24 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if position_taken?(board, index)
-    == true || index != 0..9
+  if position_taken?(board, index) == true
     return false
   else
     return true
